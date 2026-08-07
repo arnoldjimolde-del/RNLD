ed5e154c9cdca8d1678cbdc8e3c373e7172097aa
RNLD <arnoldji.molde@gmail.com>
2026-08-07 09:52:21 -0700

    UPDATED TRANSITION

---
+++ script.js
@@ -0,0 +1,10 @@
+const dropdown = document.querySelector('.dropdown');
+ 
+function hamburg() {
+  dropdown.style.transform = 'translateY(0px)';
+}
+ 
+function cancel() {
+  dropdown.style.transform = 'translateY(-500px)';
+}
+ 
